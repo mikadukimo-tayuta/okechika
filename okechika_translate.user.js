290b41d3224b03c15f828d9863df3bb5310e681b
mikadukimo-tayuta <yup.nom129@gmail.com>
2026-03-23 19:57:47 +0900

    Update okechika_translate.user.js

--- okechika_translate.user.js
+++ okechika_translate.user.js
@@ -5,7 +5,7 @@
 // @run-at       document-end
 // @updateURL    https://raw.githubusercontent.com/mikadukimo-tayuta/okechika/main/okechika_translate.user.js
 // @downloadURL  https://raw.githubusercontent.com/mikadukimo-tayuta/okechika/main/okechika_translate.user.js
-// @version      1.0.6
+// @version      1.0.7
 // ==/UserScript==
 
 (function () {

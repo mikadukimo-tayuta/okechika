c083c1c5c1ad6a5ba12b110a719624c70b9d88c0
mikadukimo-tayuta <yup.nom129@gmail.com>
2026-03-23 19:38:04 +0900

    Update okechika_translate.user.js

--- okechika_translate.user.js
+++ okechika_translate.user.js
@@ -3,9 +3,9 @@
 // @match        https://*.qtes9gu0k.xyz/*
 // @grant        none
 // @run-at       document-end
-// @updateURL    https://raw.githubusercontent.com/mikadukimo-tayuta/okechika/refs/heads/main/okechika_translate.user.js
-// @downloadURL  https://raw.githubusercontent.com/mikadukimo-tayuta/okechika/refs/heads/main/okechika_translate.user.js
-// @version      1.0.5
+// @updateURL    https://raw.githubusercontent.com/mikadukimo-tayuta/okechika/main/okechika_translate.user.js
+// @downloadURL  https://raw.githubusercontent.com/mikadukimo-tayuta/okechika/main/okechika_translate.user.js
+// @version      1.0.6
 // ==/UserScript==
 
 (function () {
